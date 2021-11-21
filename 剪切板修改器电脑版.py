52741967fcbc9bb6f442ad71d2dc37fe8e60d84a
周航山 <78966280+zhs141@users.noreply.github.com>
2021-11-21 12:41:30 +0800

    Update 剪切板修改器电脑版.py
    der

--- 剪切板修改器电脑版.py
+++ 剪切板修改器电脑版.py
@@ -4,28 +4,26 @@ import ybc_box as box
 import win32cilpboard
 
 def clipboard_get():
-    """获取剪贴板数据"""
     win32clipboard.OpenClipboard()
     data = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
     win32clipboard.CloseClipboard()
     return data
 
 def clipboard_set(data):
-    """设置剪贴板数据"""
     win32clipboard.OpenClipboard()
     win32clipboard.EmptyClipboard()
     win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, data)
     win32clipboard.CloseClipboard()
 
-op1 = box.buttonbox('What do you want to do?',['读取剪切板','修改剪切板'])
+op1 = box.buttonbox('What do you want to do?',['get','set'])
 if op1 == None:
-     box.msgbox('你未输入！自动退出！')
-elif op1 == '读取剪切板':
+     box.msgbox('None!Break!')
+elif op1 == 'get':
     kbs1 = clipboard_get()
-    pymsgbox.alart('剪切板内容',kbs1)
+    pymsgbox.alart('clipboard',kbs1)
 else:
-    kbs2 = box.enterbox('请输入你要保存的内容：')
+    kbs2 = box.enterbox('Write you want to set:')
     if kbs2 == None:
-        box.msgbox('你未输入！自动退出！')
+        box.msgbox('None!Break!')
     else:
         clipboard.set(kbs2)

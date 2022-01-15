f1dcf553f6337a3ee903d44427194c30802db440
周航山 <78966280+zhs141@users.noreply.github.com>
2022-01-15 10:41:50 +0800

    Update upload.php
    der

--- lzx/upload.php
+++ lzx/upload.php
@@ -1,1 +1,14 @@
 
+<?php
+if ($_FILES["file"]["error"] > 0)
+{
+    echo "错误：" . $_FILES["file"]["error"] . "<br>";
+}
+else
+{
+    echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
+    echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
+    echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"];
+}
+?>

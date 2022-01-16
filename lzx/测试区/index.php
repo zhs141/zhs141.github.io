96ffbdf99aaad414d0945a58f4d4548bbbb8ef0d
周航山 <78966280+zhs141@users.noreply.github.com>
2022-01-16 11:12:53 +0800

    Delete lzx directory

--- lzx/测试区/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>How to put PHP in HTML - Simple Example</title>
-  </head>
-  <body>
-    <h1><?php echo "This message is from server side." ?></h1>
-  </body>
-</html>

edd511caa636472604481d7fd65cf61918e18454
周航山 <78966280+zhs141@users.noreply.github.com>
2022-02-15 12:52:00 +0800

    Update socket服务端.py

--- socket服务端.py
+++ socket服务端.py
@@ -6,10 +6,13 @@ host = socket.gethostname()#获取本地主机名
 port = 9999
 #绑定端口号
 serversocket.bind((host,port))
+open = 1
 
 #设置最大连接数
 serversocket.listen(5)
 while 1:
+    if open == 0:
+        break
     print('服务器启动，监听中……')
     clientsocket,addr = serversocket.accept()
     puple = '连接地址：%s' % str(addr)
@@ -25,13 +28,14 @@ while 1:
         rever3 = '客户端发送内容：' + rever1
         print(rever3)
         rever2 = input('服务端发送内容' + ':')
-            print('回复：' + rever2)
-            reply = rever2.strip()
-            if not reply:
-                break
-            msg1 = time.strftime('%Y-%m-%d %X')#获取结构化时间戳
-            msg2 = '[%s]:%s'% (msg1,reply)
-            clientsocket.send(msg2.encode('utf-8'))
-        clientsocket.close()
+        print('回复：' + rever2)
+        reply = rever2.strip()
+        if not reply:
+            open = 0
+            break
+        msg1 = time.strftime('%Y-%m-%d %X')#获取结构化时间戳
+        msg2 = '[%s]:%s'% (msg1,reply)
+        clientsocket.send(msg2.encode('utf-8'))
+    clientsocket.close()
     serversocket.closel()
     print('Close!')

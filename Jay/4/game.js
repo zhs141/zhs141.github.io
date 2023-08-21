5c23215d1628e12cb205c471bafd28824fbba21a
周航山 <78966280+zhs141@users.noreply.github.com>
2023-08-21 08:58:55 +0800

    更新 game.js

--- Jay/4/game.js
+++ Jay/4/game.js
@@ -1,99 +1,68 @@
-var boardSize = 10;
-var numMines = 10;
-var board = [];
+document.addEventListener('DOMContentLoaded', () => {
+    const gameContainer = document.getElementById('game-container');
+    const dinosaur = document.getElementById('dinosaur');
+    const scoreDisplay = document.getElementById('score');
+    const jumpButton = document.getElementById('jump-button');
+    let score = 0;
+    let isJumping = false;
 
-function createBoard() {
-  for (var i = 0; i < boardSize; i++) {
-    var row = [];
-    for (var j = 0; j < boardSize; j++) {
-      row.push({
-        isMine: false,
-        isRevealed: false,
-        numAdjacentMines: 0
-      });
+    function jump() {
+        if (!isJumping) {
+            isJumping = true;
+            let position = 0;
+            let timerId = setInterval(() => {
+                if (position === 150) {
+                    clearInterval(timerId);
+                    let downTimerId = setInterval(() => {
+                        if (position === 0) {
+                            clearInterval(downTimerId);
+                            isJumping = false;
+                        }
+                        position -= 30;
+                        dinosaur.style.bottom = position + 'px';
+                    }, 20);
+                }
+                position += 30;
+                dinosaur.style.bottom = position + 'px';
+            }, 20);
+        }
     }
-    board.push(row);
-  }
-}
 
-function plantMines() {
-  var count = 0;
-  while (count < numMines) {
-    var row = Math.floor(Math.random() * boardSize);
-    var col = Math.floor(Math.random() * boardSize);
-    if (!board[row][col].isMine) {
-      board[row][col].isMine = true;
-      count++;
-    }
-  }
-}
+    jumpButton.addEventListener('click', jump);
 
-function calculateAdjacentMines() {
-  for (var i = 0; i < boardSize; i++) {
-    for (var j = 0; j < boardSize; j++) {
-      if (!board[i][j].isMine) {
-        var count = 0;
-        for (var dx = -1; dx <= 1; dx++) {
-          for (var dy = -1; dy <= 1; dy++) {
-            var newRow = i + dx;
-            var newCol = j + dy;
-            if (newRow >= 0 && newRow < boardSize && newCol >= 0 && newCol < boardSize && board[newRow][newCol].isMine) {
-              count++;
+    function generateCactus() {
+        const cactus = document.createElement('div');
+        let cactusPosition = 600;
+        let randomTime = Math.random() * 6000;
+
+        cactus.classList.add('cactus');
+        gameContainer.appendChild(cactus);
+        cactus.style.left = cactusPosition + 'px';
+
+        let timerId = setInterval(() => {
+            if (cactusPosition < -20) {
+                clearInterval(timerId);
+                gameContainer.removeChild(cactus);
+                score += 100;
+                scoreDisplay.textContent = 'Score: ' + score;
+            } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+                clearInterval(timerId);
+                gameContainer.removeChild(cactus);
+                alert('Game Over. Final Score: ' + score);
+                score = 0;
+                scoreDisplay.textContent = 'Score: ' + score;
+            } else {
+                cactusPosition -= 10;
+                cactus.style.left = cactusPosition + 'px';
             }
-          }
+        }, 20);
+
+        if (score < 10000) {
+            setTimeout(generateCactus, randomTime);
+        } else {
+            alert('Congratulations! You reached the finish line.');
         }
-        board[i][j].numAdjacentMines = count;
-      }
     }
-  }
-}
 
-function revealCell(row, col) {
-  var cell = board[row][col];
-  if (cell.isRevealed) {
-    return;
-  }
-  cell.isRevealed = true;
-
-  if (cell.isMine) {
-    alert("游戏结束，你踩到了地雷！");
-    location.reload();
-  } else if (cell.numAdjacentMines > 0) {
-    document.getElementById("cell-" + row + "-" + col).innerHTML = cell.numAdjacentMines;
-  } else {
-    document.getElementById("cell-" + row + "-" + col).style.backgroundColor = "#ccc";
-    for (var dx = -1; dx <= 1; dx++) {
-      for (var dy = -1; dy <= 1; dy++) {
-        var newRow = row + dx;
-        var newCol = col + dy;
-        if (newRow >= 0 && newRow < boardSize && newCol >= 0 && newCol < boardSize) {
-          revealCell(newRow, newCol);
-        }
-      }
-    }
-  }
-}
-
-function initGame() {
-  createBoard();
-  plantMines();
-  calculateAdjacentMines();
-
-  var boardElement = document.getElementById("board");
-  for (var i = 0; i < boardSize; i++) {
-    for (var j = 0; j < boardSize; j++) {
-      var cell = document.createElement("div");
-      cell.className = "cell";
-      cell.id = "cell-" + i + "-" + j;
-      cell.onclick = (function(row, col) {
-        return function() {
-          revealCell(row, col);
-        };
-      })(i, j);
-      boardElement.appendChild(cell);
-    }
-    boardElement.appendChild(document.createElement("br"));
-  }
-}
-
-initGame();
+    generateCactus();
+});
